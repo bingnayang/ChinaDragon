fb6371fbfe879d73c8888eb83d476f2e08fb1e15
BingNa Yang <bingnayang@gmail.com>
2020-12-16 20:03:45 -0500

    create Menu entity

---
+++ chinadragon-backend/src/main/java/com/restaurant/chinadragonbackend/entity/Menu.java
@@ -0,0 +1,27 @@
+package com.restaurant.chinadragonbackend.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="menu")
+public class Menu {
+	// Define Fields
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="section")
+	private String section;
+	
+	@Column(name="price")
+	private double price;
+}
