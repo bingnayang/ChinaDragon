a804a98dd35d1a684feb55e520e318a0ed286f2b
BingNa Yang <bingnayang@gmail.com>
2020-12-16 20:05:19 -0500

    Update Menu.java

--- chinadragon-backend/src/main/java/com/restaurant/chinadragonbackend/entity/Menu.java
+++ chinadragon-backend/src/main/java/com/restaurant/chinadragonbackend/entity/Menu.java
@@ -24,4 +24,45 @@ public class Menu {
 	
 	@Column(name="price")
 	private double price;
+	
+	public Menu() {}
+
+	public Menu(String name, String section, double price) {
+		this.name = name;
+		this.section = section;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSection() {
+		return section;
+	}
+
+	public void setSection(String section) {
+		this.section = section;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 }
