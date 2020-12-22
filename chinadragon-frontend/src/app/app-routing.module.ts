5c345372b4daf51b07e4973353ab168370c785c8
BingNa Yang <bingnayang@gmail.com>
2020-12-22 17:50:00 -0500

    creat cart button and add cart page path

--- chinadragon-frontend/src/app/app-routing.module.ts
+++ chinadragon-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { OrderOnlineComponent } from './order-online/order-online.component';
+import { CartOrderComponent } from '../app/cart-order/cart-order.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'menu', component: MenuComponent},
   {path: 'order-online', component: OrderOnlineComponent},
+  {path: 'cart', component: CartOrderComponent},
   {path: '',redirectTo: 'home', pathMatch:'full'}
 ];
 
