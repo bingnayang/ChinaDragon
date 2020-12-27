90852664c19d86b1c46917042bc4309273779f4f
BingNa Yang <bingnayang@gmail.com>
2020-12-27 13:20:57 -0500

    retrieve data to check out component

--- chinadragon-frontend/src/app/check-out/check-out.component.ts
+++ chinadragon-frontend/src/app/check-out/check-out.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderOnlineService } from '../order-online.service';
+import { CartOrder } from '../cart-order';
 
 @Component({
   selector: 'app-check-out',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./check-out.component.css']
 })
 export class CheckOutComponent implements OnInit {
+  orderItemList: CartOrder[];
+  orderSubTotal: any;
+  orderTotal: any;
+  orderTax: any;
 
-  constructor() { }
+  constructor(private orderOnlineService: OrderOnlineService) { }
 
   ngOnInit(): void {
+    this.getCartOrderList();
+    this.getCartOrderSubTotal();
+    this.getTaxAmount();
+    this.getOrderTotal();
   }
 
+  private getCartOrderList(){
+    this.orderOnlineService.getCartOrder().subscribe(data => {
+      this.orderItemList = data;
+    },error => console.log(error))
+  }
+
+  private getCartOrderSubTotal(){
+    this.orderOnlineService.calculateSubTotal().subscribe(data => {
+      this.orderSubTotal = data;
+    },error => console.log(error))
+  }
+
+  private getOrderTotal(){
+    this.orderOnlineService.calculateSubTotal().subscribe(data => {
+      this.orderTotal = data+(data * 0.08);
+    },error => console.log(error))
+  }
+
+  private getTaxAmount(){
+    this.orderOnlineService.calculateSubTotal().subscribe(data => {
+      this.orderTax = data * 0.08;
+      console.log(this.orderTax)
+    },error => console.log(error))
+  }
 }
