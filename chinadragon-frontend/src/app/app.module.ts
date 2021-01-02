df9138600aaec5b0eba47fdb1cc9a8c5b318cc91
BingNa Yang <bingnayang@gmail.com>
2021-01-01 19:21:56 -0500

    update checkout page UI and clean up code

--- chinadragon-frontend/src/app/app.module.ts
+++ chinadragon-frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,7 +10,6 @@ import { OrderOnlineComponent } from './order-online/order-online.component';
 import { CartOrderComponent } from './cart-order/cart-order.component';
 import { CheckOutComponent } from './check-out/check-out.component';
 import { ConfirmationPageComponent } from './confirmation-page/confirmation-page.component';
-
 
 @NgModule({
   declarations: [
